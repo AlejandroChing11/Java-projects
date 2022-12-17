79599876742fdf01f8e437e379e99629cd7dd176
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-17 18:04:30 -0500

    Else statement

--- HolaMundo/src/IfStatement.java
+++ HolaMundo/src/IfStatement.java
@@ -1,11 +1,13 @@
 public class IfStatement {
     public static void main(String[] args) {
-        boolean isBluetoothEnable = true;
+        boolean isBluetoothEnable = false;
         int fileSend = 3;
         if (isBluetoothEnable == true) {
             //Instructions, (send file)
             fileSend++;
-            System.out.println("Archivo enviado");
+            System.out.println("File sent");
+        } else {
+            System.out.println("Bluethoot disable");
         }
     }
 }
