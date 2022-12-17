2c96d5ba2562573c6a89af340ab9211c9d59a66a
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-16 20:42:05 -0500

    Learning CamelCase

---
+++ HolaMundo/src/CamelCase.java
@@ -0,0 +1,7 @@
+public class CamelCase {
+    public static void main(String[] args) {
+            //Lower CamelCase
+        String completeName = "Alejandro Miguel Ching Franco";
+        int sizeInCentimeters = 26;
+    }
+}
