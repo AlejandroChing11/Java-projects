a0797ca719ee7fcfcb9a4451b21110b64e693583
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-17 18:13:22 -0500

    Logic operators

---
+++ HolaMundo/src/LogicOperators.java
@@ -0,0 +1,14 @@
+public class LogicOperators {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+        System.out.println("a es igual a b?: " + (a == b));
+        System.out.println("a es diferente a b?: " + (a != b));
+
+        //Relational operators.
+        System.out.println("a es mayor que b?: " + (a > b));
+        System.out.println("a es menor que b?: " + (a < b));
+        System.out.println("a es mayor o igual que b?: " + (a >= b));
+        System.out.println("a es menor o igual que b?: " + (a <= b));
+    }
+}
