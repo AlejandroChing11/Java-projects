ef9879b120946b991c820d475690c75d47d77670
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-17 17:57:34 -0500

    If statement

---
+++ HolaMundo/src/IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isBluetoothEnable = true;
+        int fileSend = 3;
+        if (isBluetoothEnable == true) {
+            //Instructions, (send file)
+            fileSend++;
+            System.out.println("Archivo enviado");
+        }
+    }
+}
