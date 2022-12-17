e107633c0d8652dee5b9fc6e9550df0a01497df1
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-16 23:27:55 -0500

    Learning Math function

---
+++ HolaMundo/src/MathemathicsOperations.java
@@ -0,0 +1,19 @@
+public class MathemathicsOperations {
+    public static void main(String[] args) {
+        double x = 2;
+        double y = 3;
+        //Returns una parte entera hacia arriba de un tipo de dato numerico, en este caso //3.0
+        System.out.println(Math.ceil(x));
+
+        //Returns una parte entera hacia abajo de un tipo de dato numerico, en este caso //2.0
+        System.out.println(Math.floor(x));
+
+        //Returns un numero elevado a otro numero (pow)
+        System.out.println(Math.pow(x,y)); //8
+
+        //Devuelve cual de los datos es mayor
+        System.out.println(Math.max(x,y)); //3
+
+
+    }
+}
