8580629716540b30c4e260088a59b17238ac7fc8
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-16 21:18:33 -0500

    Learning Data types in java

---
+++ DataTypesJava/src/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Data Types
+        //interger
+        int n = 10; //it has a limit of 10 digits
+        //Long -- Always adding an "L" at the final of the variable.
+        long s = 1020202020213123343L;
+        //Double 8 bytes BIG
+        double d = 123.567889; //more precision
+        //Float 4 bytes it's like interger but with floats Always adding an "F" at the final of the variable.
+        float dF = 123.456F;
+    }
+}
