6971b1fd5b98094143720834fe1076d814ac7347
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-16 20:17:00 -0500

    Updating variables

---
+++ HolaMundo/src/UpdatingVariables.java
@@ -0,0 +1,14 @@
+public class UpdatingVariables {
+    public static void main(String[] args) {
+        int salary = 2000;
+        //Have recieved a bono
+        salary = salary + 200;
+        System.out.println(salary);
+        //discount 50
+        salary = salary - 50;
+        System.out.println(salary);
+        //2 extra hours and food
+        salary = salary + (30*2) - 30;
+        System.out.println(salary);
+    }
+}
