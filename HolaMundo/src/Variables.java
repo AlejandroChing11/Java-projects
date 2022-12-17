074a8bf1b8a62b59106cfd4944e10c7b7ba224e2
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-16 20:11:42 -0500

    Learning variables

---
+++ HolaMundo/src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarando una variable
+        int speed;
+        //Asignar valor a variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String nombre = "Alejandro";
+        Boolean mayor = false;
+        System.out.println(nombre);
+        System.out.println(mayor);
+        System.out.println(salary);
+    }
+}
