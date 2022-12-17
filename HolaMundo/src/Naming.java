3ed4b152f2a13af4c57528cd00d8c3824029d73b
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-16 20:30:44 -0500

    Learning Naming in java and final keyword

---
+++ HolaMundo/src/Naming.java
@@ -0,0 +1,19 @@
+public class Naming {
+    public static void main(String[] args) {
+        //Mayus sensible.
+        String $countryName = "Spain";
+        String currency$ = "COP";
+        String background_color = "Blue";
+        String nombre5 = "Alejandro";
+        System.out.println($countryName);
+        System.out.println(currency$);
+        System.out.println(background_color);
+        //For constants
+        int POSITION = -5;
+        int MAX_WIDTH = 100;
+        int MIN_WIDTH = 5;
+        final String nombre = "Alejandro";
+        System.out.println(nombre);
+        System.out.println(nombre5);
+    }
+}
