145372b05e01a3caed6c6663fcc904a6228ce6d4
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-16 21:43:14 -0500

    DataTypes vol2

--- DataTypesJava/src/DataTypes.java
+++ DataTypesJava/src/DataTypes.java
@@ -9,5 +9,20 @@ public class DataTypes {
         double d = 123.567889; //more precision
         //Float 4 bytes it's like interger but with floats Always adding an "F" at the final of the variable.
         float dF = 123.456F;
+        //Char is unicode, it can only accept a unic value 2 bytes.
+        char name = 'a';
+        //Boolean true or false 2 bytes.
+        boolean isItTrue = true;
+        //how to use var.
+        var salary = 3000; //int
+        var pension = salary * 0.03; //Double
+        var totalSalary = salary - pension;
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Alejandro";
+        System.out.println("employee name: " + employeeName + " salary: " + salary);
+
     }
 }
