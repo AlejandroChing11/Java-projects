d5670e391a519049d72f79fc3e2eaeb7c3c35332
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-17 19:55:45 -0500

    Commentarys

---
+++ HolaMundo/src/Comentarys.java
@@ -0,0 +1,16 @@
+public class Comentarys {
+    public static void main(String[] args) {
+        //Comentario tipo 1 sola linea
+
+        /*
+        Comentarios que abaracan mas de una linea
+         */
+
+        //GENERADO POR JAVA DOCS EL COMPILADOR SI LA LEE.
+        /**
+         * Functions and loops in Jav
+         * Java sdwdd
+         * Javadocs */
+
+    }
+}
