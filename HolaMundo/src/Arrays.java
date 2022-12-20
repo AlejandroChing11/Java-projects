80a14e8cd7cff809fe77ee8c8b38268f4a197653
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-20 17:19:34 -0500

    Arrays

---
+++ HolaMundo/src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] names = new String[5];
+        String days[] = new String[7];
+
+        String[][] cities = new String[4][2];
+
+        int [][][] numbers = new int[2][2][2]; //Recommendable to a Data Base
+
+
+    }
+}
