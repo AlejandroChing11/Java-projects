f29e870e759eb32461acb0b44eaa2948390a4f65
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-20 22:45:40 -0500

    Learning how to use for in arrays

--- HolaMundo/src/Arrays.java
+++ HolaMundo/src/Arrays.java
@@ -12,12 +12,16 @@ public class Arrays {
         names[2] = "Sandra";
         names[3] = "Marlon";
         names[4] = "Jenzey";
+                            //5
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
 
-        System.out.println(names[0]);
+        /*System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
         System.out.println(names[3]);
-        System.out.println(names[4]);
+        System.out.println(names[4]);*/
 
         System.out.println();
         System.out.println();
@@ -31,14 +35,23 @@ public class Arrays {
         cities[3][0] = "Venezuela";
         cities[3][1] = "Caracas";
 
-        System.out.println(cities[0][0]);
+
+        for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) { //cities[i].length quiere decir los espacios que tiene o ocupa la fila o renglon.
+                System.out.println(cities[i][j]);
+            }
+        }
+
+        /*System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
         System.out.println(cities[1][1]);
         System.out.println(cities[2][0]);
         System.out.println(cities[2][1]);
         System.out.println(cities[3][0]);
-        System.out.println(cities[3][1]);
+        System.out.println(cities[3][1]);*/
+
+
 
 
 
